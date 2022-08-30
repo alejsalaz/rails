f46125fd1dfab31f9db7e9fd7ad64a4968fa37e4
alejsalaz <alejandrosalazarzapata@gmail.com>
2022-08-30 17:17:38 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  get 'welcome/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root 'welcome#index'
+end
